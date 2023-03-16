4472f89ad51b153e5bc283c350287ae831ac8fe1
Mostafa Salah <mostafa.salah.569x@gmail.com>
2023-03-16 19:42:05 +0200

    add splash screen

---
+++ app/src/main/java/com/project/senior/splash/SplashFragment.kt
@@ -0,0 +1,29 @@
+package com.project.senior.splash
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.project.senior.R
+
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        Handler().postDelayed({
+            lifecycleScope.launchWhenResumed {
+                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToFirstFragment())
+            }
+        }, 1700)
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+}
